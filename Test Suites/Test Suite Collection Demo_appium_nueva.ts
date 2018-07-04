e1a7f0ed48db0dfa98e88064689976b3c06d69f2
liskar64 <liskar64@gmail.com>
2018-07-04 09:59:02 +0200

    Primer subida

---
+++ Test Suites/Test Suite Collection Demo_appium_nueva.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>Test Suite Collection Demo_appium_nueva</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Mobile</groupName>
+            <profileName>default</profileName>
+            <runConfigurationData>
+               <entry>
+                  <key>deviceName</key>
+                  <value>Sony D2303 (Android 5.1.1)</value>
+               </entry>
+               <entry>
+                  <key>deviceId</key>
+                  <value>ZH8006VQKP</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>Android</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Test Suite demo_appium_nueva</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
---
+++ Test Suites/Test Suite demo_appium_nueva.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite demo_appium_nueva</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-07-04T09:49:50</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8790add9-466c-4e89-90c5-603253e788be</testSuiteGuid>
+   <testCaseLink>
+      <guid>80805063-2c5e-446d-9705-dba33c1c02b3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login-OK</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
